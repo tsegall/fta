2c2d87a57659a2e27d54e1e085f3122b3e34dc59
Tim Segall <tim@cobber.com>
2019-04-12 13:19:52 -0500

    Add support for Airport Codes (IATA)

---
+++ src/main/java/com/cobber/fta/plugins/LogicalTypeIATA.java
@@ -0,0 +1,36 @@
+package com.cobber.fta.plugins;
+
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.cobber.fta.LogicalTypeFiniteSimple;
+import com.cobber.fta.PluginDefinition;
+
+/**
+ * Plugin to detect IATA Airport Codes.
+ */
+public class LogicalTypeIATA  extends LogicalTypeFiniteSimple {
+	public final static String SEMANTIC_TYPE = "AIRPORT_CODE.IATA";
+	public final static String REGEXP = "\\p{Alpha}{3}";
+	private static Set<String> members = new HashSet<String>();
+	private static String[] membersArray = null;
+
+	public LogicalTypeIATA(PluginDefinition plugin) throws FileNotFoundException {
+		super(plugin.qualifier, plugin.hotWords, REGEXP,
+				"\\p{IsAlphabetic}{3}", new InputStreamReader(LogicalTypeCAProvince.class.getResourceAsStream("/reference/IATA.txt")), 95);
+	}
+
+	@Override
+	public Set<String> getMembers() {
+		return members;
+	}
+
+	@Override
+	public String[] getMemberArray() {
+		if (membersArray == null)
+			membersArray = members.toArray(new String[members.size()]);
+		return membersArray;
+	}
+}
